4f0650853d0b0bd797ee604df899a27372130a57
Pamela Nakano <pamela.nakano@hotmail.com>
2021-08-13 21:23:04 -0300

    Remoção de comentários

--- src/main/java/com/gvendas/gestaovendas/controller/CategoriaController.java
+++ src/main/java/com/gvendas/gestaovendas/controller/CategoriaController.java
@@ -15,22 +15,21 @@ import java.util.Optional;
 
 @Api(tags = "Categoria")
 @RestController
-@RequestMapping("/categoria")  //define o caminho de acesso
+@RequestMapping("/categoria")
 public class CategoriaController {
 
     @Autowired
     private CategoriaService categoriaService;
 
     @ApiOperation(value = "Listar", nickname = "findAllCategoria")
-    @GetMapping   //"É uma consulta"
-    public List<Categoria> findAll(){
-        return  categoriaService.findAll();
+    @GetMapping
+    public List<Categoria> findAll() {
+        return categoriaService.findAll();
     }
 
     @ApiOperation(value = "Listar por código", nickname = "findByIdCategoria")
-    @GetMapping("/{id}")  //quando tem mais de um getmapping, precisa diferenciar pra não se perder nos caminhos
-    public ResponseEntity<Optional<Categoria>> findById(@PathVariable Long id){
-           //ResponseEntity para tratar pelo retorno
+    @GetMapping("/{id}")
+    public ResponseEntity<Optional<Categoria>> findById(@PathVariable Long id) {
         Optional<Categoria> categoria = categoriaService.findById(id);
         return categoria.isPresent() ? ResponseEntity.ok(categoria) : ResponseEntity.notFound().build();
     }
@@ -39,15 +38,11 @@ public class CategoriaController {
     @PostMapping
     public ResponseEntity<Categoria> save(@Valid @RequestBody Categoria categoria) {
         return ResponseEntity.status(HttpStatus.CREATED).body(categoriaService.save(categoria));
-
-//        Forma comprida de fazer:
-//        Categoria categoriaSalva = categoriaServico.salvar(categoria);
-//        return ResponseEntity.status(HttpStatus.CREATED).body(categoriaSalva);
     }
 
     @ApiOperation(value = "Atualizar", nickname = "updateCategoria")
-    @PutMapping("/{id}")                       //Variavel             //Entidade a atualizar
-    public ResponseEntity<Categoria> update(@PathVariable Long id, @Valid @RequestBody Categoria categoria){
+    @PutMapping("/{id}")
+    public ResponseEntity<Categoria> update(@PathVariable Long id, @Valid @RequestBody Categoria categoria) {
         return ResponseEntity.ok(categoriaService.update(id, categoria));
     }
 
